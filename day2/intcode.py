6b0ae892f3b9cae9ff95ba2b07428f0a286827fc
Jeffrey Work <jmcda001@github.com>
2019-12-06 08:39:40 -0800

    Day 2 completed

--- day2/intcode.py
+++ day2/intcode.py
@@ -1,18 +1,20 @@
 from typing import List
+import copy
 
 opcodes = {1: (lambda p,i: p[p[i+1]]+p[p[i+2]]),
     2: (lambda p,i: p[p[i+1]]*p[p[i+2]]),
 }
 
 def runProgram(program: List[int])->List[int]:
-    i = 0
-    while i < len(program) and program[i] != 99:
-        op = program[i]
+    pc = 0
+    ADV = 4
+    while pc < len(program) and program[pc] != 99:
+        op = program[pc]
         if op not in opcodes:
-            print(f'Error: Encountered unknown opcode {op} at index {i}')
+            print(f'Error: Encountered unknown opcode {op} at index {pc}')
             return program
-        program[program[i+3]] = opcodes[op](program,i)
-        i += 4
+        program[program[pc+3]] = opcodes[op](program,pc)
+        pc += ADV
     return program
 
 def test():
@@ -57,10 +59,21 @@ def test():
         print('Passed')
     
 if __name__ == '__main__':
-    test()
     with open('input.txt') as f:
         program = [int(i) for i in f.readline().split(',')]
-        program[1] = 12
-        program[2] = 2
+        noun = 12
+        verb = 2
+        target = 19690720
+        while True:
+            program[1] = noun
+            output = runProgram(copy.deepcopy(program))
+            if output[0] > target:
+                noun -= 1
+                break
+            noun += 1
+        program[1] = noun
+        verb = target - runProgram(copy.deepcopy(program))[0]
+        program[2] = verb
         output = runProgram(program)
         print(f'Output at index 0 after running modified program is {output[0]}')
+        print(f'100 * noun + verb? {100 * noun + verb}')
